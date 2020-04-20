18cf39ea42d92d85eaafebc6e15ee88a2910eb2a
Ege Uz <enkomaun@gmail.com>
2020-04-20 01:01:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 function App() {
+
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    async function strapiFetchTest() {
+      const response = await axios('https://majormajor-cms.herokuapp.com/tests');
+      setText(response.data[0].testfield);
+    }
+
+    strapiFetchTest();
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {text}
+      {text && " this is really exciting because we're pulling the hello world from the CMS wowowowowow"}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
