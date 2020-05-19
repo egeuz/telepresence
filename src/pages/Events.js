40c5892e1019336700da837a1bb2d8ddc41986af
Shawn Zhang <xuanaozh@newschool.edu>
2020-05-19 16:19:12 -0400

    1st try

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import '../styles/components/_events.scss';
+import backImg from '../assets/IMG_4707.00_04_06_22.Still002.jpg';
 
 function Events() {
   return (
-    <div>
-      events here
+    <div id="backgroundImg">
+      {/* background Image */}
+      <img src={backImg} className="size"/>
+
+      {/* Text Container */}
+      <div id="textBox">
+      {/* sub title */}
+      <p id="subTitle">
+      join us for a 4-day cyber expo
+      </p>
+      {/* introduction text */}
+      <p id="introDate">
+      june 11–14
+      <p id="introText">
+      a program of artist talks, workshops, and demos
+      </p>
+
+
+      </p>
+
+      </div>
+  
     </div>
   )
+
+
 }
 
 export default Events
