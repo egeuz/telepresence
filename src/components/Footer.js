d9f1adebae6e4d4e72cecdaef68f9f9874a7e50f
Ege Uz <enkomaun@gmail.com>
2020-05-25 18:40:30 -0400

    homepage hero text implemented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,12 @@ function Footer() {
   return (
     <footer>
       <div id="social-media-links">
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          instagram
-        </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          twitter
-        </a>
+      <a href="https://www.instagram.com/mfadt/" target="_blank" rel="noopener noreferrer"> instagram</a>
+      <a href="https://twitter.com/mfadt" target="_blank" rel="noopener noreferrer"> twitter </a>
       </div>
       <a href="https://www.newschool.edu/parsons/" target="_blank" rel="noopener noreferrer">
         <img src={ParsonsLogo} alt="Parsons School of Design Logo" id="parsons-logo" />
       </a>
-
     </footer>
   )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-function Gallery() {
+/* assets */
+import logotype from '../assets/logotype.svg'
+
+function Projects() {
   return (
-    <div>
-      hi this is gallery
+    <div id="projects">
+      <div id="projects-hero">
+        <div id="canvas"></div>
+        <div id="content">
+          <div id="tagline">
+            <p><span>a cyber expo</span> hosted by</p>
+            <p>first year parsons design &</p>
+            <p>technology mfa students</p>
+          </div>
+          <img id="logotype" src={logotype} alt="Telepresence logo" />
+        </div>
+      </div>
+      <div id="projects-showcase"></div>
     </div>
   )
 }
 
-export default Gallery
+export default Projects
