3e9edd3124f3da9fb07e0f84dd038fe6ed448e10
Ege Uz <enkomaun@gmail.com>
2020-05-24 22:40:33 -0400

    about page hotfix

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,26 +3,9 @@ import hero from '../assets/about-hero.png'
 
 function About() {
 
-  const [number, setNumber] = useState(0);
-
-  useEffect(() => {
-
-    const interval = setInterval(timer, 250);
-
-    function timer() {
-     //generate a random number
-     //set the random number as the number state
-    } 
-
-    return () => {
-      clearInterval(interval);
-    }
-
-  }, []);
-
   return (
-    <div id = "about-hero">
-      <div id = "content-wrapper">
+    <div id="about-hero">
+      <div id="content-wrapper">
         <h5>We are</h5>
         <h1>Creative Technologists</h1>
       </div>
