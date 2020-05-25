117bfdb42e8a7fc5884ce3967010dea26697488f
Ege Uz <enkomaun@gmail.com>
2020-05-24 22:47:44 -0400

    about hero hotfix 2

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,10 +4,12 @@ import hero from '../assets/about-hero.png'
 function About() {
 
   return (
-    <div id="about-hero">
-      <div id="content-wrapper">
-        <h5>We are</h5>
-        <h1>Creative Technologists</h1>
+    <div id="about">
+      <div id="about-hero">
+        <div id="content-wrapper">
+          <h5>We are</h5>
+          <div id="poster">Creative Technologists</div>
+        </div>
       </div>
     </div>
   )
